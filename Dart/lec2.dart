f3d7587ab7a94126ba101356cc9851dd1653ed8a
js0753 <jainam02@somaiya.edu>
2020-08-08 13:39:27 +0530

    import 'dart:io';
    main() {
      stdout.writeln("Enter Something");
      var data = stdin.readLineSync();
      print("You Entered $data");
    }

--- Dart/lec2.dart
+++ Dart/lec2.dart
@@ -1,1 +1,10 @@
+import 'dart:io';
 
+main() {
+  stdout.writeln("Enter a List of length 5");
+  List<int> l1 = new List(5);
+  for (int i = 0; i < 5; i++) l1[i] = int.parse(stdin.readLineSync());
+  int max = 0;
+  l1.forEach((e) => e > max ? max = e : max = max);
+  print("Max element of list is $max");
+}
